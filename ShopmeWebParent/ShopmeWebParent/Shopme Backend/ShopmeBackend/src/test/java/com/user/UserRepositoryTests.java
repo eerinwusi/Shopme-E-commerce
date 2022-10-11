c862b0e07a83084ff9a8c38cee1dc0b2e71aa442
Emmanuel Erinwusi <Emmanuel.Erinwusi@etranzact.com>
2022-10-11 13:37:01 +0100

    Code Count User by Id

--- ShopmeWebParent/ShopmeWebParent/Shopme Backend/ShopmeBackend/src/test/java/com/user/UserRepositoryTests.java
+++ ShopmeWebParent/ShopmeWebParent/Shopme Backend/ShopmeBackend/src/test/java/com/user/UserRepositoryTests.java
@@ -102,4 +102,13 @@ public class UserRepositoryTests {
 
         assertThat(user).isNotNull();
     }
+
+    @Test
+    public void testCountById() {
+        Integer id = 1;
+
+        Long countById = userRepository.countById(id);
+
+        assertThat(countById).isNotNull().isGreaterThan(0);
+    }
 }
